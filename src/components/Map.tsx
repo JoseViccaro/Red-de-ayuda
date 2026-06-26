0705e8c9b42b2f98830b61f67330bdd826568d91
Jose Viccaro <joseviccaro@Joses-MacBook-Air.local>
2026-06-26 22:32:12 +0200

    fix: import leaflet css directly in Map component to force dynamic styling resolution

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react';
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import { Report } from '@/types';
 
 // Asegurarse de que el CSS de Leaflet esté importado.
